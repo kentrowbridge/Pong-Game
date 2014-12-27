120fbebfe473530fa2d96fd87a1cfe26228b15ca
Kenny Trowbridge <kenny.trowbridge@gmail.com>
2014-12-26 16:32:51 -0800

    12/26 Starting ball animation

--- pongScript.js
+++ pongScript.js
@@ -2,31 +2,39 @@ $(document).ready(function(){
 	//boundary values in px
 	var topLimit = 60;
 	var botLimit = 610;
-	//var leftLimit = ;
-	//var rightLimit = ;
 	//ball calculations
 	//pixel boudnaries of paddle
-	var playerRight = $("#playerPaddle").offset().left + $("#playerPaddle").width();
-	var compLeft = $("#compPaddle").offset().left
-	//X velocity
+	var leftPaddle = $("#playerPaddle").offset().left + $("#playerPaddle").width();
+	var rightPaddle = $("#compPaddle").offset().left
 
-	//Y velocity
+	//X velocity pixels per cycle
+	var xVel = 0;
+	//Y velocity pixels per cycle
+	var yVel = 0;
+	var ballAnimation = function(){
+		var xPos = $(".ball").offset()
+	}
 
+	setInterval(ballAnimation(),100);
 	//paddle movement
-	$("#pongBoard").mousemove(function(mouse){
+	$(document).mousemove(function(mouse){
 		var paddleCenter = $("#playerPaddle").height()/2;
+		console.log("Mouse Y: "+mouse.pageY);
 		var topBound = topLimit + paddleCenter;//lowest pixel value for center of paddle
 		var botBound = botLimit - paddleCenter;//highest pixel value for center of paddle
 		//if mouse is outside those bounds, dont change anything
 		if(mouse.pageY < topBound){
+			console.log("above");
 			//snap to the top
-			$("#playerPaddle").css("top", ""+topLimit+paddleCenter);
-			$("#compPaddle").css("top", ""+topLimit+paddleCenter);
+			$("#playerPaddle").css("top", ""+topBound);
+			$("#compPaddle").css("top", ""+topBound);
 			return;
 		}
 		if(mouse.pageY > botBound){
-			$("#playerPaddle").css("top", ""+botLimit-paddleCenter);
-			$("#compPaddle").css("top", ""+botLimit-paddleCenter);
+			console.log("below");
+			//snap to the bottom
+			$("#playerPaddle").css("top", ""+botBound);
+			$("#compPaddle").css("top", ""+botBound);
 			return;
 		}
 		//set new height to center paddle at mouse Y coordinate
