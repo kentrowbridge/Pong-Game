eb0535ca0a59f4f8c08ac8f0baa99b0197ccbde1
Kenny Trowbridge <kenny.trowbridge@gmail.com>
2014-12-29 13:20:48 -0800

    12/29 working game

--- pongScript.js
+++ pongScript.js
@@ -1,69 +1,139 @@
 $(document).ready(function(){
-	//boundary values in px
-	var topLimit = Math.floor($("#pongBoard").offset().top);
-	var botLimit = Math.floor(topLimit+$("#pongBoard").height());
-	//ball calculations
-	//pixel boudnaries of board
-	var leftBound = Math.floor($("#pongBoard").offset().left);
-	var rightBound = Math.floor($("#pongBoard").offset().left + $("#pongBoard").width());
+	var $player = $("#playerPaddle");
+	var $computer = $("#compPaddle");
+	var $board = $("#pongBoard");	
+	var $ball = $(".ball");
 
+	var animation;
+	var intervalspd = 10;
 	//XY Velocity in pixels per cycle
-	var xVel = 10;
-	var yVel = 0;
-
-	//start ball animation, once every 0.1s	
-	var count= 0;
-	var ball = $(".ball");
-	var animation = setInterval(ballAnimation, 10);
-
+	var xVel;
+	var yVel;
+	var xPos;
+	var yPos;
+	$ball.css("top", yPos);
+	$ball.css("left", xPos);
+	//start ball animation
+	var animation;
+	//boundary values in px
+	var topLimit = Math.floor($board.offset().top);
+	var botLimit = Math.floor(topLimit+$board.height());
+	//ball calculations
+	startAnimation();
 	/*
 	 * Function that handles the movement of the ball
 	 */
 	function ballAnimation(){
-		//ball is moving left
-		// if (xVel < 0) {
-		// 	console.log("Ball is moving left");
-		// 	if(ball.offset().left <= leftBound){//ball hits left wall
-		// 		console.log("Hit left wall");
-		// 		xVel = -xVel;//reverse x vector
-		// 	}
-		// } else if (xVel > 0) {//ball moving right
-		// 	console.log("Ball moving right");
-		// 	if((ball.offset().left + ball.width()) >= rightBound){//ball hits right wall
-		// 		console.log("Hit right wall");
-		// 		xVel = -xVel;//reverse x vector
-		// 	}
-		// }
-		var nextX = ball.offset().left + xVel;
-		var nextY = ball.offset().top+yVel;
-		ball.css("left", ""+nextX);
-		console.log("It moved");
+		getBounds();
+		console.log("animation running");
+		//horizontal bounds		
+		if (xVel < 0) {//ball is moving left
+			if($ball.position().left <= $player.width()){//ball hits left wall
+				if(hitPlayer()){
+					xVel = -xVel;
+				} else {
+					alert("Out of bounds left");
+					clearInterval(animation);
+					animation = false;
+				}
+			}
+		} else if (xVel > 0) {//ball moving right
+			if(($ball.position().left + $ball.width()) >= ($board.width() - $computer.width())){//ball hits right wall
+				if(hitComp()){
+					xVel = -xVel;
+				} else {
+					alert("Out of bounds right");
+					clearInterval(animation);
+					animation = false;
+				}
+			}
+		}
 
+		// //vertical bounds
+		if (yVel < 0) {//ball is moving up
+			if($ball.position().top <= 0){//ball hits left wall
+				yVel = -yVel;//reverse x vector
+			}
+		} else if (yVel > 0) {//ball moving down
+			if(($ball.position().top + $ball.height()) >= $board.height()){//ball hits right wall
+				yVel = -yVel;//reverse x vector
+			}
+		}
+		//move ball
+		$ball.css("left", $ball.position().left+xVel);
+		$ball.css("top", $ball.position().top+yVel);
 	};
+
+	function hitPlayer(){
+		if($ball.position().top >= $player.position().top &&
+			$ball.position().top <= ($player.position().top + $player.height() - $ball.height())){		
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function startAnimation(){
+		//XY Velocity in pixels per cycle
+		xVel = (Math.random()*5);
+		yVel = (Math.random()*5);
+		xPos = (Math.random()*($board.width()/2))+($board.width()/4);
+		yPos = Math.random()*$board.height();
+		$ball.css("top", yPos);
+		$ball.css("left", xPos);
+		//start ball animation
+		animation = setInterval(ballAnimation, intervalspd);
+	}
+
+	function hitComp(){
+		if($ball.position().top >= $computer.position().top &&
+			$ball.position().top <= ($computer.position().top + $computer.height() - $ball.height())){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	//function used to make sure the ball will react only with present window size
+	function getBounds(){
+		//boundary values in px
+		topLimit = Math.floor($("#pongBoard").offset().top);
+		botLimit = Math.floor(topLimit+$("#pongBoard").height());
+		//ball calculations
+		//pixel boudnaries of board
+		leftBound = Math.floor($("#pongBoard").offset().left);
+		rightBound = Math.floor($("#pongBoard").offset().left + $("#pongBoard").width());
+	}
 
 	//paddle movement
 	$(document).mousemove(function(mouse){
-		var paddleCenter = $("#playerPaddle").height()/2;
+		var paddleCenter = $player.height()/2;
 		var topBound = topLimit + paddleCenter;//lowest pixel value for center of paddle
 		var botBound = botLimit - paddleCenter;//highest pixel value for center of paddle
 		//if mouse is outside those bounds, dont change anything
 		if(mouse.pageY < topBound){
 			//snap to the top
-			$("#playerPaddle").css("top", ""+topBound);
-			$("#compPaddle").css("top", ""+topBound);
+			$player.css("top", ""+topBound);
+			$computer.css("top", ""+topBound);
 			return;
 		}
 		if(mouse.pageY > botBound){
 			//snap to the bottom
-			$("#playerPaddle").css("top", ""+botBound);
-			$("#compPaddle").css("top", ""+mouse.pageY);
+			$player.css("top", ""+botBound);
+			$computer.css("top", ""+mouse.pageY);
 			return;
 		}
 		//set new height to center paddle at mouse Y coordinate
-		$("#playerPaddle").css("top", ""+mouse.pageY-paddleCenter-topLimit);
-		$("#compPaddle").css("top", ""+mouse.pageY-paddleCenter-topLimit);
+		$player.css("top", ""+mouse.pageY-paddleCenter-topLimit);
+		$computer.css("top", ""+mouse.pageY-paddleCenter-topLimit);
 	});
+
 	$(document).click(function(){
-		clearInterval(animation);
+		if(!animation){
+			startAnimation();
+		} else {
+			clearInterval(animation);
+			animation = false;
+		}
 	});
 });
